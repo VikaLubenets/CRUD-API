785169f84f7445644d4477e7c188a719bb186d5c
VikaLubenets <lubenetcvika@gmail.com>
2024-10-19 21:52:42 +0200

    fix: fix bug with put method

--- src/services/updateUser.ts
+++ src/services/updateUser.ts
@@ -1,7 +1,16 @@
 import { IncomingMessage } from "node:http";
+import { User } from "../types";
 
 export default function UpdateUser(req: IncomingMessage, userIndex: number) {
-    const updatedUserData = req.users;
+    const updatedUserData = req.body as Partial<User>;
+
+    if (updatedUserData.age) {
+        const parsedAge = Number(updatedUserData.age);
+
+        if (!isNaN(parsedAge)) {
+            updatedUserData.age = parsedAge;
+        }
+    }
 
     req.users[userIndex] = {
         ...req.users[userIndex],
