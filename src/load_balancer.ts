deefcbabe6dad231295ec31d37600539ff19a65d
VikaLubenets <lubenetcvika@gmail.com>
2024-10-21 20:02:32 +0200

    feat: add parallellism of api requests

--- src/load_balancer.ts
+++ src/load_balancer.ts
@@ -1,13 +1,13 @@
+import 'dotenv/config'
 import http from 'node:http';
 import cluster from 'node:cluster';
 import os from 'node:os';
-import * as dotenv from 'dotenv';
 import { createServerInstance } from './server';
 
-dotenv.config();
 
 const numCPUs = os.cpus().length - 1;
 const PORT = Number(process.env.PORT) || 4000;
+const BASE_URL = 'http://localhost/'
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
@@ -16,62 +16,99 @@ if (cluster.isPrimary) {
     cluster.fork();
   }
 
+  const workers = cluster.workers ? Object.values(cluster.workers) : [];
+
   cluster.on('exit', (worker) => {
-    console.log(`Worker ${worker.process.pid} is died. We are going to  start a new one`);
-    cluster.fork();
+    console.log(`Worker ${worker.process.pid} has died. Starting a new one.`);
+    const newWorker = cluster.fork();
+    workers[worker.id] = newWorker;
   });
 
   let currentIndex = 0;
-  const workers = cluster.workers ? Object.values(cluster.workers) : [];
+  const server = http.createServer((req, res) => {
+      const redirectTo = new URL(BASE_URL)
+      redirectTo.pathname = req.url || ''
+      redirectTo.port = String(PORT + (currentIndex++ % numCPUs))
+      res.writeHead(307, { Location: redirectTo.href })
+      res.end()
+    });
 
-  const loadBalancer = http.createServer((req, res) => {
-    const worker = workers[currentIndex];
-    if (worker) {
-        const { method, headers, url } = req;
-        let body: any = '';
-  
-        req.on('data', (chunk) => {
-          body += chunk;
-        });
-  
-        req.on('end', () => {
-          worker.send({ method, headers, url, body }, (err) => {
-            if (err) {
-              console.error('Error sending request to worker:', err);
-              res.writeHead(500);
-              res.end('Internal Server Error');
-            }
-          });
-        
-          console.log(`Request ${url} received on load balancer, forwarding to worker id ${worker.id}`);
-          
-          worker.once('message', (message) => {
-            console.log(`Worker ${worker.id} responded with message:`, message);
-            const { statusCode, headers, responseBody } = message;
-            res.writeHead(statusCode, headers);
-            res.end(responseBody);
-          });
-        });
-      }
-    currentIndex = (currentIndex + 1) % workers.length;
-  });
-
-  loadBalancer.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Load balancer listening on port ${PORT}`);
   });
 
 } else {
-    const workerId = cluster.worker?.id;
-    if (workerId) {
-        const workerPort = PORT + workerId;
-        const workerServer = http.createServer((req, res) => {
-          createServerInstance();
-        });
-        
-        workerServer.listen(workerPort, () => {
-          console.log(`Worker with id ${workerId} and pid ${process.pid} started on port ${workerPort}`);
-        });
-    }
+  const workerId = cluster.worker?.id;
+
+  if (workerId) {
+    const port = PORT + workerId;
+    const workerServer = createServerInstance();
+
+    workerServer.listen(port, () => {
+      console.log(`Worker with id ${workerId} and pid ${process.pid} started on port ${port}`);
+    });
+
+    workerServer.on('request', (req) => {
+      console.log(
+          `Worker:${port} received request: ${req.method}:${req.url}`,
+      )
+  });
+  }
 }
+
+
+
+
+
+
+// import http from 'node:http';
+// import cluster from 'node:cluster';
+// import os from 'node:os';
+// import * as dotenv from 'dotenv';
+// import {createServerInstance} from './server';
+
+// dotenv.config();
+
+// const numCPUs = os.cpus().length - 1;
+// const PORT = Number(process.env.PORT) || 4000;
+
+// if (cluster.isPrimary) {
+//   console.log(`Primary ${process.pid} is running`);
+
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('exit', (worker) => {
+//     console.log(`Worker ${worker.process.pid} has died. Starting a new one.`);
+//     cluster.fork();
+//   });
+
+//   let currentIndex = 0;
+//   const workers = cluster.workers ? Object.values(cluster.workers) : [];
+
+//   const loadBalancer = http.createServer((req, res) => {
+//     const worker = workers[currentIndex];
+//     if (worker) {
+      
+    
+//     }
+//     currentIndex = (currentIndex + 1) % workers.length;
+//   });
+
+//   loadBalancer.listen(PORT, () => {
+//     console.log(`Load balancer listening on port ${PORT}`);
+//   });
+
+// } else {
+//   const workerId = cluster.worker?.id;
+//   if (workerId) {
+//     const workerServer = createServerInstance();
+
+//     workerServer.listen(PORT + workerId, () => {
+//       console.log(`Worker with id ${workerId} and pid ${process.pid} started on port ${PORT + workerId}`);
+//     });
+//   }
+// }
 
 
